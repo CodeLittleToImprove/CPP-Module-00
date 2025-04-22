cdf9304099ce8cf39fc21bd834e0affcdc5c4f60
Thang Bui Quoc <tbuiloves42@gmail.com>
2025-04-22 16:02:22 +0200

    added change to add cmd

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -4,11 +4,34 @@
 
 int main()
 {
-	Contact phonebook[8];
-	for (int i = 0; i < 8; ++i)
+	std::string command;
+	Contact contacts[8];
+	int contactCount = 0;
+	int currentIndex = 0;
+	while (true)
 	{
-		std::cout << "\nAdding contact #" << i + 1 << std::endl;
-		addContact(phonebook[i]);
-	}
+		std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+		std::getline(std::cin, command);
 
+		if (command == "ADD")
+		{
+			if (contactCount < 8)
+			{
+				std::cout << "adding contact #" << (contactCount + 1) << std::endl;
+				addContact(contacts[currentIndex]);
+				contactCount++;
+			}
+			else
+			{
+				std::cout << "Phonebook is full! Replacing oldest contact." << std::endl;
+				addContact(contacts[currentIndex]);
+			}
+			currentIndex = (currentIndex + 1) % 8;
+		}
+//	for (int i = 0; i < 8; ++i)
+//	{
+//		std::cout << "\nAdding contact #" << i + 1 << std::endl;
+//		addContact(phonebook[i]);
+//	}
+	}
 }
