107ed01fec5a38469107e7d49a1a5edda37d6908
tbui-quo <tbui-quo@student.42wolfsburg.de>
2025-04-24 16:13:12 +0200

    final push

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui-quo <tbui-quo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 16:12:42 by tbui-quo          #+#    #+#             */
+/*   Updated: 2025/04/24 16:12:58 by tbui-quo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-#include "Contact.hpp"
+# include "Contact.hpp"
 
 # define TESTMODE false
 # define ENABLE_STRICT_INPUT true
+
 class PhoneBook
 {
 private:
